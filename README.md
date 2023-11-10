7a9dfdfbf313dc459ab54de8db4471f74d9d74f1
travolgi <travolgi@gmail.com>
2023-11-10 11:01:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Advice Generator App
+
+This is a simple Advice Generator App using Axios
+![Advice Generator App](./assets/img/design.jpg)
+
+## Table of contents
+
+- [Built with](#built-with)
+- [Useful resources](#useful-resources)
+
+## Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Grid
+- Mobile-first workflow
+- Accessibility first workflow
+- [Axios](https://axios-http.com/docs/intro) - JS library
+
+## Useful resources
+
+- [Axios](https://axios-http.com/docs/intro) - docs
---
+++ assets/main.js
@@ -0,0 +1,36 @@
+const url = 'https://api.adviceslip.com/advice';
+
+window.onload = () => {
+   const card = document.body.querySelector('article');
+   const adviceId = document.body.querySelector('#adviceId');
+   const adviceText = document.body.querySelector('#advice');
+   const btnNewAdvice = document.body.querySelector('#getNewAdvice');
+
+   const randomColor = () => {
+      let letters = '0123456789ABCDEF'.split('');
+      let color = '#';
+      for (let i=0; i < 6; i++) {
+         color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+   }
+
+   async function getAdvice() {
+      try {
+         card.classList.remove('zoom');
+
+         const res = await axios.get(url);
+         let adviceSlip = res.data.slip;
+         
+         adviceId.innerText = adviceSlip.id;
+         adviceText.innerText = adviceSlip.advice;
+         document.body.style.backgroundColor = randomColor();
+
+         card.classList.add('zoom');
+      } catch (err) {
+         console.error(err);
+      }
+   }
+   
+   btnNewAdvice.addEventListener('click', getAdvice);
+}
